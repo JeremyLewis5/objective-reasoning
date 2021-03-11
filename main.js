f87ed98534c453823e97af7c49be8a46f512a23a
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-11 09:04:09 -0600

    dsfg

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function isAdmin(str){
+    if (str.userRole === 'ADMIN') {
+        return true
+    } else {
+        return false
+    }
+}
+
+function getEmail(str) {
+    const first = firstName;
+    const last = lastName;
+    const arr1 = [firstName + '.' + lastName];
+    if (arr1){
+    return arr1 + '@codeimmersives.com'
+    }
+}
+// const arr1 = [{firstName}, {lastName}];
+// console.log(arr1)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
